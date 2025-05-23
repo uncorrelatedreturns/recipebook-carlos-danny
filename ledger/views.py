4858cdf6c6a1bd3c0a13c2f95820c93738a9a2ca
uncorrelatedreturns <danny.carlos@student.ateneo.edu>
2025-05-23 16:57:34 +0800

    made login required for recipe list and detail views

--- ledger/views.py
+++ ledger/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Recipe
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, "ledger/recipe_list.html", {"recipes": recipes})
 # Example access using hint: Ingredient.objects.filter(recipe__recipe__name="<Name of Recipe>")
 
-
+@login_required
 def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     return render(request, "ledger/recipe_detail.html", {"recipe": recipe})
