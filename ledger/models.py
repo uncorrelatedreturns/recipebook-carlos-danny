0380a73be81656ffb147aec3b9c2ad69c1139bed
uncorrelatedreturns <danny.carlos@student.ateneo.edu>
2025-05-23 15:37:35 +0800

    Set the string representation and absolute urls of the Ingredient and Recipe models

--- ledger/models.py
+++ ledger/models.py
@@ -4,10 +4,21 @@ from django.urls import reverse
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("ingredient_detail", args=[str(self.id)])
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("recipe_detail", args=[str(self.id)])
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=255)
@@ -15,3 +26,5 @@ class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
 
 
+    def __str__(self):
+        return f"{self.quantity} of {self.ingredient.name} in {self.recipe.name}"
