9cf6e1527d037bdd69eb5ba114317f0d9d1d0b19
uncorrelatedreturns <danny.carlos@student.ateneo.edu>
2025-05-23 15:36:39 +0800

    Added the 3 models, and its name, quantity and recipe fields

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=255)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipe")
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
+
+
