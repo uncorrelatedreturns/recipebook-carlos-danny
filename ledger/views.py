f9901301b93ca3643b819134d5419496c7f8b633
uncorrelatedreturns <danny.carlos@student.ateneo.edu>
2025-05-23 14:50:04 +0800

    updated the recipes to my favorite foods

--- ledger/views.py
+++ ledger/views.py
@@ -3,29 +3,36 @@ from django.shortcuts import render
 def recipe_list(request):
     recipes = [
         {"name": "Pork Sinigang", "link": "/recipe/1"},
-        {"name": "Recipe 2: Bicol Express", "link": "/recipe/2"},
+        {"name": "Bicol Express", "link": "/recipe/2"},
     ]
     return render(request, "ledger/recipe_list.html", {"recipes": recipes})
 
 def recipe_1(request):
-    ingredients = [
-        {"name": "pork belly", "quantity": "1kg"},
-        {"name": "tamarind soup base mix", "quantity": "1 packet"},
-        {"name": "water", "quantity": "1.5L},
-        {"name": "tomatoes", "quantity": "3pcs"},
-        {"name": "onion", "quantity": "1pc"},
-        {"name": "kangkong", "quantity": "1 bunch"},
-    ]
-    return render(request, "ledger/recipe_detail.html", {"title": "Pork Sinigang", "ingredients": ingredients})
+    context = {
+        "title": "Pork Sinigang",
+        "ingredients": [
+            {"name": "pork belly", "quantity": "1kg"},
+            {"name": "tamarind soup base mix", "quantity": "1 packet"},
+            {"name": "water", "quantity": "1.5L"},
+            {"name": "tomatoes", "quantity": "3pcs"},
+            {"name": "onion", "quantity": "1pc"},
+            {"name": "kangkong", "quantity": "1 bunch"},
+            {"name": "sitaw (string beans)", "quantity": "1 cup"},
+        ]
+    }
+    return render(request, "ledger/recipe_detail.html", context)
 
 def recipe_2(request):
-    ingredients = [
-        {"name": "pork belly", "quantity": "1kg"},
-        {"name": "coconut milk", "quantity": "2 cups"},
-        {"name": "coconut cream", "quantity": "1 cup"},
-        {"name": "onion", "quantity": "1pc"},
-        {"name": "garlic", "quantity": "5 cloves"},
-        {"name": "shrimp paste (bagoong)", "quantity": "2 tablespoons"},
-        {"name": "green chili peppers", "quantity": "3pcs"},
-    ]
-    return render(request, "ledger/recipe_detail.html", {"title": "Recipe 2 -- Bicol Express", "ingredients": ingredients})
+    context = {
+        "title": "Bicol Express",
+        "ingredients": [
+            {"name": "pork belly", "quantity": "1kg"},
+            {"name": "coconut milk", "quantity": "2 cups"},
+            {"name": "coconut cream", "quantity": "1 cup"},
+            {"name": "onion", "quantity": "1pc"},
+            {"name": "garlic", "quantity": "5 cloves"},
+            {"name": "shrimp paste (bagoong)", "quantity": "2 tablespoons"},
+            {"name": "green chili peppers", "quantity": "3pcs"},
+        ]
+    }
+    return render(request, "ledger/recipe_detail.html", context)
