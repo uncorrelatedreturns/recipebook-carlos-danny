5d549924cb2f3419f618b6d64948e7d9198e3072
uncorrelatedreturns <danny.carlos@student.ateneo.edu>
2025-05-23 14:10:05 +0800

    hardcoded the necessary context for the recipe list and recipe 1 and 2

--- ledger/views.py
+++ ledger/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+def recipe_list(request):
+    recipes = [
+        {"name": "Recipe 1", "link": "/recipe/1"},
+        {"name": "Recipe 2", "link": "/recipe/2"},
+    ]
+    return render(request, 'ledger/recipe_list.html', {'recipes': recipes})
+
+def recipe_1(request):
+    ingredients = [
+        {"name": "tomato", "quantity": "3pcs"},
+        {"name": "onion", "quantity": "1pc"},
+        {"name": "pork", "quantity": "1kg"},
+        {"name": "water", "quantity": "1L"},
+        {"name": "sinigang mix", "quantity": "1 packet"},
+    ]
+    return render(request, 'ledger/recipe_detail.html', {'title': "Recipe 1", 'ingredients': ingredients})
+
+def recipe_2(request):
+    ingredients = [
+        {"name": "garlic", "quantity": "1 head"},
+        {"name": "onion", "quantity": "1pc"},
+        {"name": "vinegar", "quantity": "1/2cup"},
+        {"name": "water", "quantity": "1 cup"},
+        {"name": "salt", "quantity": "1 tablespoon"},
+        {"name": "whole black peppers", "quantity": "1 tablespoon"},
+        {"name": "pork", "quantity": "1 kilo"},
+    ]
+    return render(request, 'ledger/recipe_detail.html', {'title': "Recipe 2", 'ingredients': ingredients})
