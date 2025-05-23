0ba802db89af952225da885b2a22c6a7350437a2
uncorrelatedreturns <danny.carlos@student.ateneo.edu>
2025-05-23 16:39:58 +0800

    Added a Profile model to extend User with name and bio fields

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
@@ -28,3 +29,11 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f"{self.quantity} of {self.ingredient.name} in {self.recipe.name}"
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
