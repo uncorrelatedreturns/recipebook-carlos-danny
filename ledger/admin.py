adec0faf6f095ff631fbc7f9aae81db59a71fc1f
uncorrelatedreturns <danny.carlos@student.ateneo.edu>
2025-05-23 15:45:13 +0800

    registered models to admin with inline editing and list display

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient
 
-# Register your models here.
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    inlines = [RecipeIngredientInline]
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ("recipe", "ingredient", "quantity")
+    list_filter = ("recipe", "ingredient")
