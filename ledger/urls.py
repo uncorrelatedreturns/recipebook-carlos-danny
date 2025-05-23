b0f0c616e075805cee4f57e2e968867d4fb75258
uncorrelatedreturns <danny.carlos@student.ateneo.edu>
2025-05-23 21:36:49 +0800

    fixed url issue

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('recipes/list', views.recipe_list, name='recipe_list'),
-    path('recipe/<int:pk>', views.recipe_detail, name='recipe_detail'),
+    path('recipes/list/', views.recipe_list, name='recipe_list'),
+    path('recipe/<int:pk>/', views.recipe_detail, name='recipe_detail'),
     path('login/', views.custom_login, name='login'),
     path('logout/', views.custom_logout, name='logout'),
     path('recipe/add/', views.add_recipe, name='add_recipe'),
