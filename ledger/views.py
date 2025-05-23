1bfabcfabc8efa7ed4e92398bed13b479f038181
uncorrelatedreturns <danny.carlos@student.ateneo.edu>
2025-05-23 14:43:33 +0800

    test

--- ledger/views.py
+++ ledger/views.py
@@ -2,29 +2,30 @@ from django.shortcuts import render
 
 def recipe_list(request):
     recipes = [
-        {"name": "Recipe 1", "link": "/recipe/1"},
-        {"name": "Recipe 2", "link": "/recipe/2"},
+        {"name": "Pork Sinigang", "link": "/recipe/1"},
+        {"name": "Recipe 2: Bicol Express", "link": "/recipe/2"},
     ]
-    return render(request, 'ledger/recipe_list.html', {'recipes': recipes})
+    return render(request, "ledger/recipe_list.html", {"recipes": recipes})
 
 def recipe_1(request):
     ingredients = [
-        {"name": "tomato", "quantity": "3pcs"},
+        {"name": "pork belly", "quantity": "1kg"},
+        {"name": "tamarind soup base mix", "quantity": "1 packet"},
+        {"name": "water", "quantity": "1.5L},
+        {"name": "tomatoes", "quantity": "3pcs"},
         {"name": "onion", "quantity": "1pc"},
-        {"name": "pork", "quantity": "1kg"},
-        {"name": "water", "quantity": "1L"},
-        {"name": "sinigang mix", "quantity": "1 packet"},
+        {"name": "kangkong", "quantity": "1 bunch"},
     ]
-    return render(request, 'ledger/recipe_detail.html', {'title': "Recipe 1", 'ingredients': ingredients})
+    return render(request, "ledger/recipe_detail.html", {"title": "Pork Sinigang", "ingredients": ingredients})
 
 def recipe_2(request):
     ingredients = [
-        {"name": "garlic", "quantity": "1 head"},
+        {"name": "pork belly", "quantity": "1kg"},
+        {"name": "coconut milk", "quantity": "2 cups"},
+        {"name": "coconut cream", "quantity": "1 cup"},
         {"name": "onion", "quantity": "1pc"},
-        {"name": "vinegar", "quantity": "1/2cup"},
-        {"name": "water", "quantity": "1 cup"},
-        {"name": "salt", "quantity": "1 tablespoon"},
-        {"name": "whole black peppers", "quantity": "1 tablespoon"},
-        {"name": "pork", "quantity": "1 kilo"},
+        {"name": "garlic", "quantity": "5 cloves"},
+        {"name": "shrimp paste (bagoong)", "quantity": "2 tablespoons"},
+        {"name": "green chili peppers", "quantity": "3pcs"},
     ]
-    return render(request, 'ledger/recipe_detail.html', {'title': "Recipe 2", 'ingredients': ingredients})
+    return render(request, "ledger/recipe_detail.html", {"title": "Recipe 2 -- Bicol Express", "ingredients": ingredients})
